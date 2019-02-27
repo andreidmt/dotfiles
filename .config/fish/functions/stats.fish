97cf370e37a316f46bd7f8fcf1557bbd7a33a4e7
Andrei Dumitrescu <5057797+andreidcm@users.noreply.github.com>
2019-02-27 22:24:11 +0100

    Add script for network name and strength

--- .config/fish/functions/stats.fish
+++ .config/fish/functions/stats.fish
@@ -1,45 +1,49 @@
 function stats -a cmd -d "~> i3blocks stats"
     switch "$cmd"
-        case "pacman"
+        # Wireless network name and strength
+        case "wifi"
+            set name (wpa_cli status | grep "^ssid" | awk '{split($0,a,"="); print a[2]}')
+            set strength (cat /proc/net/wireless | awk 'NR==3 {print $3 "00%"}')
+            echo $name - $strength
+        # Top proceses by cpu usage
+		case "cpu"
+        	ps axch -o cmd:15,pid:10,%cpu --sort=-%cpu | head
+		
+		# Top processes by mem usage
+		case "mem"
+			ps axch -o cmd:15,pid:10,%mem --sort=-%mem | head
+        
+		# How many packages waiting for update
+		case "pacman"
             set package_count (pacman -Qu | wc | awk '{print $1}')
 
             echo "$package_count packages"
 
-            # update i3blocks if running 
+            # i3blocks red 
             if pgrep -f i3blocks > /dev/null
                 if test $package_count -gt 0
                     return 33
                 end
             end
+
+		# CPU temperature
         case "cpu-temp"
             set temp (acpi -t | awk '{print $4 }')
-            set usage (mpstat 1 1 | awk '$3 ~ /CPU/ { for(i=1;i<=NF;i++) { if ($i ~ /%idle/) field=i } } $3 ~ /all/ { printf("%d",100 - $field) }')
-
-            echo $usage $temp"°C"
             
-            # update i3blocks if running 
+            echo $temp"°C"
+            
+            # i3blocks red
             if pgrep -f i3blocks > /dev/null
-                if test $temp -gt 60
+                if test $temp -gt 65
                     return 33
                 end
             end
+
+		# Memory user / memory available
         case "memory"
-            awk '
-            /^MemTotal:/ {
-                mem_total=$2
-            }
-            /^MemFree:/ {
-                mem_free=$2
-            }
-            /^Buffers:/ {
-                mem_free+=$2
-            }
-            /^Cached:/ {
-                mem_free+=$2
-            }
-            END {
-                printf("%.2fG/%.2fG\n", (mem_total - mem_free)/1024/1024, mem_total/1024/1024)
-            }' /proc/meminfo
+            free -h | awk '/^Mem:/ {print $3 "/" $2}'
+
+		# Battery percent state and time left till empty or full (if charging)
         case "battery"
             set charge (acpi -b | awk '{print $4}' | sed 's/[^0-9.]*//g')
             set bstatus (acpi -b | awk '{print $3}')
@@ -47,7 +51,7 @@ function stats -a cmd -d "~> i3blocks stats"
 
             echo $charge"%" $time
 
-            # update i3blocks if running 
+            # i3blocks red 
             if pgrep -f i3blocks > /dev/null
                 if test \( $bstatus = "Discharging," \) -a \( $charge -lt 20 \)
                     return 33
