2066bc344e7e97c0a6ae2415be4700cf2d68c174
Andrei Dumitrescu <5057797+andreidcm@users.noreply.github.com>
2019-03-01 10:30:53 +0100

    Connect to vpn after wifi connect

--- .config/fish/functions/io.fish
+++ .config/fish/functions/io.fish
@@ -1,5 +1,8 @@
 function io -a cmd -a subcmd -d "~> IO" 
     switch "$cmd"
+        ##
+        ## Start ssh-agent if not running, add keys so ssh knows what to use
+        ##    
         case "ssh-add"
             if not pgrep -f ssh-agent > /dev/null
                 echo "No running ssh-agent, starting one."
@@ -8,14 +11,34 @@ function io -a cmd -a subcmd -d "~> IO"
 
             ssh-add ~/.ssh/github_t420
             ssh-add ~/.ssh/asd14__nginx--tiny-1
+            
+        ##
+        ## Wifi interaction
+        ##
         case "wifi"
             switch "$subcmd"
+                # Wireless speed test
                 case "speed"
                     speedometer -r wlp3s0
+
+                # Connect to home network
                 case "home"
                     sudo netctl start wifi-112357
+
+                # Choose wifi network
                 case "*"
                     sudo wifi-menu
             end
+            
+            # connect to fastest vpn server
+            sudo pvpn -f
+        
+        case "*"
+            notify "~> IO" "command \"$cmd\" is invalid"
+    end
+
+    # Update i3blocks 
+    if pgrep -f i3blocks > /dev/null
+        pkill -SIGRTMIN+12 i3blocks
     end
 end
