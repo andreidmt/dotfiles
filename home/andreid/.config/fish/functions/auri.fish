78e397d9e82be2d9f2d156a3396c8237bb2998f0
Andrei Dumitrescu <5057797+andreidmt@users.noreply.github.com>
2021-06-07 00:09:07 +0200

    refactor: cleanup and moving around

--- home/andreid/.config/fish/functions/auri.fish
+++
@@ -1,5 +0,0 @@
-function auri -a pkg_name  --description "Install AUR package"
-    set url https://aur.archlinux.org/cgit/aur.git/snapshot/"$pkg_name".tar.gz 
-    
-    curl -sO "$url" && tar -xvf "$pkg_name".tar.gz && cd "$pkg_name" && makepkg --noconfirm -si && cd .. && rm -rf "$pkg_name" "$pkg_name".tar.gz ;
-end
