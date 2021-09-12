7a8c3d9da6bb07cf4eca228f1acedc3d6ea0f4fb
Andrei Dumitrescu <5057797+andreidmt@users.noreply.github.com>
2021-09-12 16:08:07 +0200

    feat: add zsh install script

---
+++ install.zsh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# Exit script if any statement returns a non-true return value
+set -e
+
+# Exit script if trying to use an uninitialised variable
+set -u
+
+scriptFolder=$(dirname `realpath $0`)
+
+cp -r "$scriptFolder/.config/zsh" "$HOME/.config/zsh"
+
+ln -s "$HOME/.config/zsh/.zshrc" "$HOME/.zshrc"
