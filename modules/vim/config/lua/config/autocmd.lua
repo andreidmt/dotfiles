1ec5fc41a7b980770230e7162afdc30683615ae1
Andrei Dumitrescu <5057797+andreidmt@users.noreply.github.com>
2024-10-06 22:10:42 +0200

    feat(vim): add conceal to improve markdown readability

--- modules/vim/config/lua/config/autocmd.lua
+++ modules/vim/config/lua/config/autocmd.lua
@@ -1,47 +1,56 @@
 local vim_utils = require("utils.vim")
 
--- Neovim Lua configuration for auto-formatting Lua with stylua on save
+-- Hide parts of markdown syntax for easier reading
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = "markdown",
+	callback = function()
+		-- Concealed text is completely hidden.
+		vim.opt_local.conceallevel = 3
+	end,
+})
+
+-- NeoVim Lua configuration for auto-formatting Lua with stylua on save
 vim.api.nvim_create_augroup("FormatAutogroup", { clear = true })
 vim.api.nvim_create_autocmd("BufWritePost", {
-  group = "FormatAutogroup",
-  pattern = "*.lua",
-  command = "silent! !stylua %",
+	group = "FormatAutogroup",
+	pattern = "*.lua",
+	command = "silent! !stylua %",
 })
 
 -- Define filetypes and their patterns
 vim_utils.create_autocmds({
-  html = "*.ejs",
-  markdown = "*.mdx",
-  zsh = ".zshrc",
-  bash = ".xinitrc",
-  groff = ".1",
-  javascript = {
-    "*.js.flow",
-    "*.flow"
-  },
-  json = {
-    ".d41rc",
-    ".c8rc",
-    ".zerobootrc",
-    ".remarkrc",
-    ".browserslistrc",
-    ".commitlintrc",
-    ".eslintrc",
-    ".lintstagedrc",
-    ".markdownlintrc",
-    ".prettierrc",
-    ".swcrc",
-    ".releaserc",
-    "*.tfstate",
-    "*.tfstate.backup"
-  },
-  hcl = {
-    "*.hcl",
-    ".terraformrc",
-    "terraform.rc"
-  },
-  terraform = {
-    "*.tf",
-    "*.tfvars"
-  },
+	html = "*.ejs",
+	markdown = "*.mdx",
+	zsh = ".zshrc",
+	bash = ".xinitrc",
+	groff = ".1",
+	javascript = {
+		"*.js.flow",
+		"*.flow",
+	},
+	json = {
+		".d41rc",
+		".c8rc",
+		".zerobootrc",
+		".remarkrc",
+		".browserslistrc",
+		".commitlintrc",
+		".eslintrc",
+		".lintstagedrc",
+		".markdownlintrc",
+		".prettierrc",
+		".swcrc",
+		".releaserc",
+		"*.tfstate",
+		"*.tfstate.backup",
+	},
+	hcl = {
+		"*.hcl",
+		".terraformrc",
+		"terraform.rc",
+	},
+	terraform = {
+		"*.tf",
+		"*.tfvars",
+	},
 })
