44580444bcf730546281b94b0d1f96395f9979ec
Andrei Dumitrescu <5057797+andreidmt@users.noreply.github.com>
2025-01-13 10:14:39 +0100

    feat(vim): update YAML LSP server with GitLab CI schema

--- modules/vim/config/lua/plugins/lsp-servers/yaml.lua
+++ modules/vim/config/lua/plugins/lsp-servers/yaml.lua
@@ -6,15 +6,13 @@ return function(on_attach)
     on_attach = on_attach,
     settings = {
       yaml = {
-        schemas = {
-          ['https://json.schemastore.org/circleciconfig.json'] = '.circleci/**/*.{yml,yaml}',
-          ['https://raw.githubusercontent.com/lalcebo/json-schema/master/serverless/reference.json'] =
-          'serverless.{yml,yaml}'
-        }
-      }
+        ["https://json.schemastore.org/circleciconfig.json"] = ".circleci/**/*.{yml,yaml}",
+        ["https://raw.githubusercontent.com/lalcebo/json-schema/master/serverless/reference.json"] = "serverless.{yml,yaml}",
+        ["https://gitlab.com/gitlab-org/gitlab/-/blob/master/app/assets/javascripts/editor/schema/ci.json"] = ".gitlab-ci.{yml,yaml}",
+      },
     },
     flags = {
-      debounce_text_changes = 150
+      debounce_text_changes = 150,
     },
   })
 end
