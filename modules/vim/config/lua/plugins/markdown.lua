c570cf72226f6bab05984ea4a1af14f51dfe2c46
Andrei Dumitrescu <5057797+andreidmt@users.noreply.github.com>
2025-05-12 13:51:14 +0200

    feat(vim): config vim-markdown-toc

--- modules/vim/config/lua/plugins/markdown.lua
+++ modules/vim/config/lua/plugins/markdown.lua
@@ -10,36 +10,40 @@ vim.api.nvim_create_autocmd("FileType", {
 return {
   {
     "mzlogin/vim-markdown-toc",
-    event = { "BufEnter" },
     ft = { "markdown" },
-  },
-  {
-    "iamcco/markdown-preview.nvim",
-    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
     event = { "BufEnter" },
-    ft = { "markdown" },
-    config = function()
-      vim.g.mkdp_browser = "nyxt"
+    init = function()
+      vim.g.vmt_list_item_char = "-"
+      vim.g.vmt_list_indent_text = "  "
     end,
-    build = "cd app && yarn install",
   },
-  -- {
-  --   "MeanderingProgrammer/render-markdown.nvim",
-  --   opts = {
-  --     code = {
-  --       -- Determines how code blocks & inline code are rendered:
-  --       --  none:     disables all rendering
-  --       --  normal:   adds highlight group to code blocks & inline code, adds
-  --       --            padding to code blocks
-  --       --  language: adds language icon to sign column if enabled and icon +
-  --       --            name above code blocks
-  --       --  full:     normal + language
-  --       style = "add",
-  --     }
-  --   },
-  --   dependencies = {
-  --     "nvim-treesitter/nvim-treesitter",
-  --     "nvim-tree/nvim-web-devicons"
-  --   },
-  -- }
+	{
+		"iamcco/markdown-preview.nvim",
+		cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+		event = { "BufEnter" },
+		ft = { "markdown" },
+		config = function()
+			vim.g.mkdp_browser = "nyxt"
+		end,
+		build = "cd app && yarn install",
+	},
+	-- {
+	--   "MeanderingProgrammer/render-markdown.nvim",
+	--   opts = {
+	--     code = {
+	--       -- Determines how code blocks & inline code are rendered:
+	--       --  none:     disables all rendering
+	--       --  normal:   adds highlight group to code blocks & inline code, adds
+	--       --            padding to code blocks
+	--       --  language: adds language icon to sign column if enabled and icon +
+	--       --            name above code blocks
+	--       --  full:     normal + language
+	--       style = "add",
+	--     }
+	--   },
+	--   dependencies = {
+	--     "nvim-treesitter/nvim-treesitter",
+	--     "nvim-tree/nvim-web-devicons"
+	--   },
+	-- }
 }
