3f625178364eff685a48f5bfd1f7d6604dd3d4cd
Andrei Dumitrescu <5057797+andreidmt@users.noreply.github.com>
2025-05-17 10:08:03 +0200

    chore: format

--- modules/vim/config/lua/plugins/markdown.lua
+++ modules/vim/config/lua/plugins/markdown.lua
@@ -17,33 +17,33 @@ return {
       vim.g.vmt_list_indent_text = "  "
     end,
   },
-	{
-		"iamcco/markdown-preview.nvim",
-		cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-		event = { "BufEnter" },
-		ft = { "markdown" },
-		config = function()
-			vim.g.mkdp_browser = "nyxt"
-		end,
-		build = "cd app && yarn install",
-	},
-	-- {
-	--   "MeanderingProgrammer/render-markdown.nvim",
-	--   opts = {
-	--     code = {
-	--       -- Determines how code blocks & inline code are rendered:
-	--       --  none:     disables all rendering
-	--       --  normal:   adds highlight group to code blocks & inline code, adds
-	--       --            padding to code blocks
-	--       --  language: adds language icon to sign column if enabled and icon +
-	--       --            name above code blocks
-	--       --  full:     normal + language
-	--       style = "add",
-	--     }
-	--   },
-	--   dependencies = {
-	--     "nvim-treesitter/nvim-treesitter",
-	--     "nvim-tree/nvim-web-devicons"
-	--   },
-	-- }
+  {
+    "iamcco/markdown-preview.nvim",
+    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+    event = { "BufEnter" },
+    ft = { "markdown" },
+    config = function()
+      vim.g.mkdp_browser = "nyxt"
+    end,
+    build = "cd app && yarn install",
+  },
+  -- {
+  --   "MeanderingProgrammer/render-markdown.nvim",
+  --   opts = {
+  --     code = {
+  --       -- Determines how code blocks & inline code are rendered:
+  --       --  none:     disables all rendering
+  --       --  normal:   adds highlight group to code blocks & inline code, adds
+  --       --            padding to code blocks
+  --       --  language: adds language icon to sign column if enabled and icon +
+  --       --            name above code blocks
+  --       --  full:     normal + language
+  --       style = "add",
+  --     }
+  --   },
+  --   dependencies = {
+  --     "nvim-treesitter/nvim-treesitter",
+  --     "nvim-tree/nvim-web-devicons"
+  --   },
+  -- }
 }
