10cb9872572316b4280f4f5e9925973348f05fab
Andrei Dumitrescu <5057797+andreidmt@users.noreply.github.com>
2025-05-29 16:53:10 +0200

    feat(vim): add python to lsp config

--- modules/vim/config/lua/plugins/lsp-servers/python.lua
+++ modules/vim/config/lua/plugins/lsp-servers/python.lua
@@ -3,15 +3,30 @@ return function(on_attach)
 
   lspconfig.pylsp.setup({
     on_attach = on_attach,
+    cmd = { "pylsp" },
     settings = {
       pylsp = {
+        configurationSources = { "flake8" },
         plugins = {
-          pycodestyle = {
-            ignore = { "W391" },
-            maxLineLength = 80,
+          jedi = {
+            environment = vim.fn.getcwd() .. "/.venv/bin/python",
           },
+          black = { enabled = true, line_length = 99 },
+          isort = { enabled = true, profile = "black" },
+          pylama = { enabled = true },
+          pycodestyle = { enabled = false },
+          mccabe = { enabled = false },
+          pyflakes = { enabled = false },
+          flake8 = { enabled = false },
+          rope_completion = { enabled = true },
+          rope_autoimport = { enabled = true },
         },
       },
     },
+    root_dir = lspconfig.util.root_pattern(
+      "pyproject.toml",
+      "setup.py",
+      ".git"
+    ),
   })
 end
