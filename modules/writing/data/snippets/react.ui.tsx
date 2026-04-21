a0abe64fe71e77f31296fd89dbfe6448a2d196eb
Andrei Dumitrescu <5057797+andreidmt@users.noreply.github.com>
2026-04-21 20:36:50 +0200

    feat(writing): add barebone react comp

--- modules/writing/data/snippets/react.ui.tsx
+++ modules/writing/data/snippets/react.ui.tsx
@@ -1,13 +1,12 @@
-import { FC, memo } from 'react';
+import type { FC } from "react"
 
-const debug = require('debug')('namespace:core.ui/ui');
+type ComponentProps = {
+  text?: string
+}
 
-type UIPropsType = {
-	text?: string;
-};
+const Component: FC<ComponentProps> = ({ text = undefined }) => {
+  return <div>{text}</div>
+}
 
-export const UI: FC<UIPropsType> = memo(({ text }) => {
-	debug({ text });
-
-	return <div>{text}</div>;
-});
+export { Component }
+export type { ComponentProps }
